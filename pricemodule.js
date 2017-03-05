ccf42e0b510f51f4795071e1abf0b68645f050ba
ramboxeu <mail.rambox@gmail.com>
2017-03-05 17:28:07 +0100

    Add files via upload

---
+++ pricemodule.js
@@ -0,0 +1,14 @@
+const Ceny = (function(){
+  const polerzepak = 100;
+  const polezborze = 100;
+  const traktoryrzepak = 1000;
+  const traktoryzborze = 1000;
+  const swinka = 10;
+  return {
+    poleRzepak : polerzepak,
+    poleZborze : polezborze,
+    traktoryRzepak : traktoryrzepak,
+    traktoryZborze : traktoryzborze,
+    swinka : swinka
+  }
+})();
